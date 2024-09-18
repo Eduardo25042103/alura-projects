a28808289e64340fd324f9ad3a979d9510b06b37
Eduardo <124465964+Eduardo25042103@users.noreply.github.com>
2024-09-18 05:09:36 +0000

    tengo sueño

--- passgenerator/main.js
+++ passgenerator/main.js
@@ -1,5 +1,17 @@
+let pass_length = document.getElementById('cantidad');
+let btn = document.getElementById('generar');
 
+const char_password = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
 
+function generatePassword()
+{
+    let number = parseInt(pass_length.value);
+
+    if(number < 8)
+    {
+        alert("Lenght min: 8");  
+    }  
+}
 
 
 
